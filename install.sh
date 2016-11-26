9768d8db5db2440d0a2144ef309423e264cfb911
fred-penot <frederic.penot@gmail.com>
2016-11-26 22:43:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ cd /home/${login_ssh}/HomeUtils
 sh install.sh
 chmod -Rf 777 /home/${login_ssh}/HomeUtils
 chown -R ${login_ssh}:${login_ssh} /home/${login_ssh}/HomeUtils
+
+mkdir /root/.ionic
+echo '{' > /root/.ionic/ionic.config
+echo '  "ionicServeAddress": "172.17.0.2"' >> /root/.ionic/ionic.config
+echo '}' >> /root/.ionic/ionic.config
+chmod -Rf 755 /root/.ionic
