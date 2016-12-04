6a8ae28508ddf2fcaa7ef39f7e7928eecadb72f5
fred-penot <frederic.penot@gmail.com>
2016-12-04 17:06:05 +0100

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,5 +1,6 @@
 login_ssh="docker"
 
-service ssh start
+service ssh start > /home/${login_ssh}/services.log
 
-sh /home/${login_ssh}/HomeUtils/ionic serve
+sh /home/${login_ssh}/HomeUtils/ionic serve >> /home/${login_ssh}/services.log
+chmod -f 777 /home/${login_ssh}/services.log
